57c27e981eda39c24eba91531c316d5bb47302d4
Joshua Homer <countjoshington@gmail.com>
2013-09-11 13:35:13 -0600

    adding needed files

---
+++ application/views/footer.php
@@ -0,0 +1,6 @@
+<div id="footer">
+  &copy; 2013 <a href="http://www.baconwafflestudios.com">BaconWaffle Studios</a> All Rights Reserved.
+ </div>
+ </div>
+</body>
+</html>
---
+++ application/views/thank.php
@@ -0,0 +1,14 @@
+<div id="content">
+<div class="signup_wrap">
+<div class="signin_form">
+ <?php echo form_open("index/login"); ?>
+  <label for="email">Email:</label>
+  <input type="text" id="email" name="email" value="" />
+  <label for="pass">Password:</label>
+  <input type="password" id="pass" name="pass" value="" />
+  <input type="submit" class="" value="Sign in" />
+ <?php echo form_close(); ?>
+</div>
+</div>
+<h1>Thank you for registering! Use the form above to log in to your new account!</h1>
+</div>
