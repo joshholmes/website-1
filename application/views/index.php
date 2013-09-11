47d78cb4aa404d8a4622e22758a22ea0020b6adb
Joshua Homer <countjoshington@gmail.com>
2013-09-11 14:02:32 -0600

    bugfix test code

--- application/views/index.php
+++ application/views/index.php
@@ -2,4 +2,5 @@
   <h2>Welcome Back, <?php echo $this->session->userdata('username'); ?>!</h2>
   <p>This section represents the area that only logged in members can access.</p>
   <h4><?php echo anchor('index/logout', 'Logout'); ?></h4>
+  <p>DEGUB: <?php echo 'id: ' . $data['user']['id'] . ' | username: ' . $data['user']['username'] . ' | password: ' . $data['user']['password'];?></p>
 </div><!--<div class="content">-->
