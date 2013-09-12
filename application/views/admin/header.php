1ef4e7ef3ca8928e72b521aa9099e7cbd4d15dea
Joshua Homer <countjoshington@gmail.com>
2013-09-12 11:30:08 -0600

    static file folders added, added jquery call to header files

--- application/views/admin/header.php
+++ application/views/admin/header.php
@@ -4,6 +4,7 @@
  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
  <title><?php echo (isset($title)) ? $title : "" ?> </title>
  <link rel="stylesheet" type="text/css" href="<?php echo base_url('static/css/style.css') ?>" />
+ <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
 </head>
 <body>
  <div id="wrapper">
