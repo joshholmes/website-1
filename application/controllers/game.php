42f13905f2917e7a3e9394c88f44754bbafca3e0
Joshua Homer <countjoshington@gmail.com>
2013-09-13 15:03:35 -0600

    added game stuff, will work more later

---
+++ application/controllers/game.php
@@ -0,0 +1,27 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Game extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('usermodel');
+	}
+	
+	public function index()
+	{
+		if($this->usermodel->check_admin($this->session->userdata('username')))
+		{
+			$data['admin'] = true;
+			$this->load->view('header', $data);
+			$this->load->view('game/game', $data);
+			$this->load->view('footer', $data);			
+		} else
+		{
+			$data['admin'] = false;
+			$this->load->view('header', $data);
+			$this->load->view('home', $data);
+			$this->load->view('footer', $data);
+		}
+	}
+}
---
+++ application/views/game/game.php
@@ -0,0 +1,4 @@
+<div class="content">
+  <p>This section represents the area that only logged in administrators can access.</p>
+  <h4><?php echo anchor('home/logout', 'Logout'); ?></h4>
+</div>
