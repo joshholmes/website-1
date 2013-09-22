c4b26473a3fbb151a13413e27143fd4b4cc401f2
Felix Milea-Ciobanu <felixmilea@gmail.com>
2013-09-21 21:41:28 -0400

    add .project to .gitignore (since its system/IDE specific and not relevant to code base)

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'chaoticf_game';
-$db['default']['password'] = 'BdTF72@N]o5{';
-$db['default']['database'] = 'chaoticf_browser_game';
+$db['default']['username'] = 'root';
+$db['default']['password'] = '';
+$db['default']['database'] = 'baconwaffle';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
